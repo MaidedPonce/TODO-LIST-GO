b90b86aef27e17059e71c5fe439201f6158b7883
MaidedPonce <maided.h.p90@gmail.com>
2022-06-15 19:08:06 -0500

    adding abstraction

--- src/repository/repository.go
+++ src/repository/repository.go
@@ -1,10 +1,24 @@
 package repository
 
-import tasks "github.com/MaidedPonce/TODO-LIST-GO/src/models"
+import (
+	"context"
+
+	tasks "github.com/MaidedPonce/TODO-LIST-GO/src/models"
+)
 
 type Repository interface {
 	ReadTaskID(id string) tasks.Task
 	ReadTasks() tasks.List
 	CreateTask(task tasks.Task) (tasks.Task, error)
 	DeleteTask(id string)
+}
+
+var implementation Repository
+
+func SetRepository(repo Repository) {
+	implementation = repo
+}
+
+func ReadTaskID(ctx context.Context, id string) (*tasks.Task, error) {
+	return implementation.ReadTasks(ctx, id)
 }
